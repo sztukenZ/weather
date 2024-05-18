52a10536415cdc244e6db4c393c12647e89d4afc
krzysztofsztuk <krzysztof.jerzy.sztuk@gmail.com>
2024-05-18 11:09:22 +0200

    Almost finished

--- main/views.py
+++ main/views.py
@@ -10,4 +10,24 @@ app_id = os.environ.get('API_KEY')
 def index(request):
     if request.method == 'POST':
         city = request.POST['city']
-        appid = 
+        
+        source = urllib.request.urlopen(
+            'https://api.openweathermap.org/data/3.0/weather?q =' 
+            + city + f'&appid = {app_id}'
+        ).read()
+
+        list_of_data = json.loads(source)
+
+        data = {
+            "country_code": str(list_of_data['sys']['country']),
+            "coordinate": str(list_of_data['coord']['lon']) + ' ' 
+                + str(list_of_data['coord']['lat']),
+            "temp": str(list_of_data['mian']['temp']) + 'k',
+            "pressure": str(list_of_data["main"]["pressure"]),
+            "humidity": str(list_of_data["main"]["humidity"])
+        }
+        print(data)
+    else:
+        data = {}
+    return render(request, "main/index.html", data)
+    
